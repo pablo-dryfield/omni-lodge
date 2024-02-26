35f0a51b4cc0362f5cff138aa510fbfb4441e5f5
pablo.cabrera <58521451+pablo-dryfield@users.noreply.github.com>
2024-02-26 19:38:00 +0100

    Fixed Login and dummy info implemented

--- ui/src/pages/Login.tsx
+++ ui/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent } from 'react';
-import { TextInput, PasswordInput, Button, Paper, Title, Container, Group } from '@mantine/core';
+import { TextInput, PasswordInput, Button, Paper, Title, Container, Group, Checkbox, Anchor, Avatar, Center } from '@mantine/core';
 import { IconUser, IconLock } from '@tabler/icons-react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { setPasswordState, setUserState } from '../actions/sessionActions';
@@ -11,39 +11,44 @@ const LoginPage: React.FC = () => {
 
     const handleLogin = async (event: React.FormEvent) => {
         event.preventDefault();
-        const data = await dispatch(loginUser({email:user, password:password}));
+        // Assuming your loginUser action creator expects an object with email and password
+        await dispatch(loginUser({ email: /*user*/ 'rob@t5social.com', password: /*password*/ '12341234' }));
     };
 
     return (
-        <Container size={420} my={40}>
-            <Paper radius={12} p="xl" withBorder style={{ textAlign: 'center' }}>
-                <Title order={2} mb="lg">
-                    Login
+        <Container size={420} my={40} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+            <Paper radius={12} p="xl" withBorder style={{ width: 340, textAlign: 'center' }}>
+                <Center>
+                    <Avatar variant="filled" radius="xl" size="xl" color="#4088F6" src="" />
+                </Center>
+                <Title order={2} style={{ marginBottom: 20, paddingTop: 30 }}>
+                    Sign In
                 </Title>
-
-                <form onSubmit={handleLogin}>
+                <form onSubmit={handleLogin} style={{ textAlign: 'left' }}>
                     <TextInput
-                        label="Username or Email"
-                        placeholder="Username or Email"
+                        label="Username"
+                        placeholder="Username"
                         value={user}
                         onChange={(event: ChangeEvent<HTMLInputElement>) => dispatch(setUserState(event.target.value))}
                         leftSection={<IconUser />}
-                        required
+                        //required
                     />
-
                     <PasswordInput
                         label="Password"
                         placeholder="Password"
                         value={password}
                         onChange={(event: ChangeEvent<HTMLInputElement>) => dispatch(setPasswordState(event.target.value))}
                         leftSection={<IconLock />}
-                        required
+                        //required
                         mt="md"
                     />
-
-                    <Group mt="xl" style={{ justifyContent: 'flex-end' }}>
-                        <Button type="submit">Login</Button>
+                    <Group mt="md">
+                        <Checkbox label="Remember Me" />
+                        <Anchor href="#" size="sm" onClick={(event) => event.preventDefault()}>Forgot Password?</Anchor>
                     </Group>
+                    <Button fullWidth mt="xl" type="submit">
+                        Login
+                    </Button>
                 </form>
             </Paper>
         </Container>
