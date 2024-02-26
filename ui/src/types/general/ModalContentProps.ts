7eca127b56a751b58e1715206b74610523171549
pablo.cabrera <58521451+pablo-dryfield@users.noreply.github.com>
2024-02-26 21:17:13 +0100

    Re-organized types paths and created func

---
+++ ui/src/types/general/ModalContentProps.ts
@@ -0,0 +1,10 @@
+import { MRT_RowData, MRT_Row, useMantineReactTable } from "mantine-react-table";
+
+export type ModalContentProps<T extends MRT_RowData> = {
+    internalEditComponents: React.ReactNode[];
+    row: MRT_Row<T>;
+    table: ReturnType<typeof useMantineReactTable<T>>;
+    opened: boolean;
+    setOpened: (opened: boolean) => void;
+    title: string;
+}
