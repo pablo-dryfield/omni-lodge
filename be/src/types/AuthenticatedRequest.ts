8a198fdd6c347ee9797df9d20d1e27c244348a59
pablo-dryfield <58521451+pablo-dryfield@users.noreply.github.com>
2025-10-22 18:43:44 +0200

    Update AuthenticatedRequest.ts

--- be/src/types/AuthenticatedRequest.ts
+++ be/src/types/AuthenticatedRequest.ts
@@ -11,5 +11,6 @@ export interface AuthenticatedRequest extends Request {
   user?: string | JwtPayload;
   authContext?: AuthorizationContext;
   permissionCache?: Map<string, Set<string>>;
+  file?: Express.Multer.File;
+  files?: Express.Multer.File[] | Record<string, Express.Multer.File[]>;
 }
-
