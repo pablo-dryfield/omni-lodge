8caa11f7e1f5e5f95ae3a75f74da94892723cbb0
pablo-dryfield <58521451+pablo-dryfield@users.noreply.github.com>
2025-12-03 12:34:17 +0100

    update

--- be/src/services/bookings/parsers/index.ts
+++ be/src/services/bookings/parsers/index.ts
@@ -2,9 +2,11 @@ import type { BookingEmailParser } from '../types.js';
 import { BasicBookingParser } from './basicBookingParser.js';
 import { FareHarborBookingParser } from './fareHarborBookingParser.js';
 import { GetYourGuideBookingParser } from './getYourGuideBookingParser.js';
+import { ViatorBookingParser } from './viatorBookingParser.js';
 
 const parserInstances: BookingEmailParser[] = [
   new GetYourGuideBookingParser(),
+  new ViatorBookingParser(),
   new FareHarborBookingParser(),
   new BasicBookingParser(),
 ];
